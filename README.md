4f139f772cfee85189898b01b8f8e78d841cfff0
Shaun Donnelly <shaundonnelly@gmail.com>
2018-04-28 15:51:47 +0100

    new stuff

--- README.md
+++ README.md
@@ -13,3 +13,11 @@ Then make a copy of `config.example.json`, named `config.json`.
 ```
 python tweet.py
 ```
+
+## Packaging it up for Lambda
+
+You need dependencies to be installed at the top level, so:
+
+```
+pip install -r requirements.txt -t .
+```
--- tweet.py
+++ tweet.py
@@ -2,19 +2,21 @@ import json
 import tweepy
 import markov
 
-config = json.load(open('config.json'))
-twitterConfig = config['twitter']['credentials']
-consumerKey = twitterConfig['consumer_key']
-consumerSecret = twitterConfig['consumer_secret']
-accessTokenKey = twitterConfig['access_token_key']
-accessTokenSecret = twitterConfig['access_token_secret']
+def main(foo, bar):
+    config = json.load(open('config.json'))
+    twitterConfig = config['twitter']['credentials']
+    consumerKey = twitterConfig['consumer_key']
+    consumerSecret = twitterConfig['consumer_secret']
+    accessTokenKey = twitterConfig['access_token_key']
+    accessTokenSecret = twitterConfig['access_token_secret']
 
-auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
-auth.set_access_token(accessTokenKey, accessTokenSecret)
-twitterClient = tweepy.API(auth)
+    auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
+    auth.set_access_token(accessTokenKey, accessTokenSecret)
+    twitterClient = tweepy.API(auth)
 
-tweet_to_send = markov.get_tweet()
-twitterClient.update_status(status=tweet_to_send)
+    tweet_to_send = markov.get_tweet()
+    print "tweeting " + tweet_to_send
+    twitterClient.update_status(status=tweet_to_send)
 
 
 
